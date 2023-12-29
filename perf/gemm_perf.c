6423809711edfb645880e35077413132e8ee4aaa
jsn <yx546@cornell.edu>
2023-12-28 19:59:16 -0500

    perf: optimize and compare GEMM execution metrics

--- perf/gemm_perf.c
+++ perf/gemm_perf.c
@@ -1,38 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "kernel/gemm/gemm.h"
+#include "../kernel/gemm/gemm.h"
 #include "helpers.h"
 
-float *generate_random_matrix(int rows, int cols) {
-    float *matrix = (float *)malloc(rows * cols * sizeof(float));
+double *generate_random_matrix(int rows, int cols) {
+    double *matrix = (double *)malloc(rows * cols * sizeof(double));
     for (int i = 0; i < rows * cols; i++) {
-        matrix[i] = (float)rand() / RAND_MAX;
+        matrix[i] = (double)rand() / (double)RAND_MAX;
     }
     return matrix;
 }
 
-void free_matrix(float *matrix) {
+void free_matrix(double *matrix) {
     free(matrix);
 }
 
 int main() {
     srand(time(NULL));
 
-    FILE *f = fopen("./bench/gflops_v1.txt", "w");
+    FILE *f = fopen("./bench/gemm_4x4_v16.txt", "w");
     if (f == NULL) {
         printf("Error opening file!\n");
         exit(1);
     }
 
-    for (int N = 1; N <= 1000; N++) {
-        float *A = generate_random_matrix(N, N);
-        float *B = generate_random_matrix(N, N);
-        float *C = (float *)malloc(N * N * sizeof(float));
+    for (int N = 40; N <= 1000; N+=40) {
+        double *A = generate_random_matrix(N, N);
+        double *B = generate_random_matrix(N, N);
+        double *C = (double *)malloc(N * N * sizeof(double));
 
         printf("Performing multiplication for N = %d...\n", N);
         uint64_t start = nanos();
-        gemm_v1(N, N, N, A, N, B, N, C, N);
+        gemm_4x4_v16(N, N, N, A, N, B, N, C, N);
         uint64_t end = nanos();
 
         double gflop = (2.0 * N * N * N) * 1e-9;
--- plot.py
+++ plot.py
@@ -2,30 +2,67 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
+import numpy as np
+import os
 
 # Set the aesthetic style of the plots
-sns.set(style="whitegrid")
+sns.set(style="ticks", palette="pastel")
 
-# Read the data from the file
-N_values = []
-gflops_values = []
-with open('gflops_results.txt', 'r') as file:
-    for line in file:
-        N, gflops = line.split(',')
-        N_values.append(int(N))
-        gflops_values.append(float(gflops))
+# Path to the 'bench' directory
+bench_path = './bench'
+
+# Initialize a DataFrame to store all data
+all_data = pd.DataFrame()
+
+# List all files in the 'bench' directory and read their contents into a DataFrame
+for filename in os.listdir(bench_path):
+    if filename.endswith('.txt'):
+        file_path = os.path.join(bench_path, filename)
+        # Read data from each file
+        data = pd.read_csv(file_path, header=None, names=['N', filename], index_col=0)
+        # Interpolate missing values for a smoother line
+        data = data.reindex(range(data.index.min(), data.index.max())).interpolate()
+        all_data = pd.concat([all_data, data], axis=1)
+
+# Sort columns by the performance at N=1000 (or the closest available value)
+# We take the value for N=1000 or interpolate if it doesn't exist
+performance_at_1000 = all_data.loc[all_data.index.get_loc(1000, method='nearest')].sort_values(ascending=False)
+sorted_columns = performance_at_1000.index
 
 # Plot the data
-plt.figure(figsize=(10, 6))
-plt.plot(N_values, gflops_values, marker='x', markersize=5, linewidth=2, label="GFLOPS vs N")
-plt.title('GFLOPS vs N')
-plt.xlabel('N')
-plt.ylabel('GFLOPS')
-plt.legend()
-plt.grid(True)
+fig, ax = plt.subplots(figsize=(10, 6))  # Adjust the size of the figure
 
-# Save the plot
-plt.savefig("gflops_vs_N.png", dpi=300)  # High quality
+# Generate a color palette that's visually distinct
+colors = plt.cm.tab20(np.linspace(0, 1, len(sorted_columns)))
 
-# Display the plot
+# Plot each column with the sorted colors
+for col, color in zip(sorted_columns, colors):
+    ax.plot(all_data.index, all_data[col], label=col, color=color)
+
+# Customize the plot to make it publication-quality
+ax.set_title('GFLOPS Performance by Benchmark', fontsize=16, weight='bold')
+ax.set_xlabel('Matrix Size N', fontsize=14, weight='bold')
+ax.set_ylabel('GFLOPS', fontsize=14, weight='bold')
+
+# Adjust the position of the legend and the plot
+box = ax.get_position()
+ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])  # Resize plot to make space for the legend
+legend = ax.legend(title='Benchmarks', bbox_to_anchor=(1.05, 1), loc='upper left', fontsize=10)
+plt.setp(legend.get_title(), fontsize='12')  # Set the fontsize of the legend's title
+
+ax.grid(True, which='both', linestyle='--', linewidth=0.5)
+ax.tick_params(axis='both', which='major', labelsize=12)
+
+# Set the tick labels to be outside
+sns.despine(trim=True, offset=10)
+
+# Save the figure
+fig_path = 'gflops_performance.png'
+plt.savefig(fig_path, format='png', dpi=300, bbox_inches='tight')
+
+# Show the plot
 plt.show()
+
+# Return the path to the saved figure
+fig_path
